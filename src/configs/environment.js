542deaee6a4d8752e791c32ca5b66ee54da40fde
yanu-setiawan <yanusetiawan777@gmail.com>
2023-03-07 01:37:44 +0700

    v-

--- src/configs/environment.js
+++ src/configs/environment.js
@@ -1,8 +1,8 @@
 module.exports = {
-  host: process.env.HOST,
-  db: process.env.DB,
+  host: process.env.DB_HOST,
+  db: process.env.DB_DATABASE,
   dbPort: process.env.DB_PORT,
-  user: process.env.UNAME,
-  pwd: process.env.PWD,
+  user: process.env.DB_USER,
+  pwd: process.env.DB_PWD,
   serverPort: process.env.SERVER_PORT,
 };
