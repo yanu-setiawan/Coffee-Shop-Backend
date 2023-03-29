1388836777d50aab109959a26250294e7dbeb6ef
yanu-setiawan <yanusetiawan777@gmail.com>
2023-03-29 17:04:28 +0700

    order

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -50,8 +50,6 @@ const getProducts = (query) => {
     let order = "p.id ASC";
     if (query.order === "cheapest") order = "p.price ASC";
     if (query.order === "priciest") order = "p.price DESC";
-    if (query.order === "newest") order = "p.id DESC";
-    if (query.order === "latest") order = "p.id ASC";
 
     sqlQuery += ` ORDER BY ${order}`;
 
